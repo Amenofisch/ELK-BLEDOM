be964a1278c01bb1b2f9ba8531e567273964f4b9
Amenofisch <51821196+Amenofisch@users.noreply.github.com>
2022-08-27 15:21:11 +0200

    Added eslint and finally did 'npm init'

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	'env': {
+		'commonjs': true,
+		'es2021': true,
+		'node': true
+	},
+	'extends': 'eslint:recommended',
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'rules': {
+		'indent': [
+			'warn',
+			'spaces'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
