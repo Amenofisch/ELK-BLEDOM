1c1dd33ab32b34190f018b299e6ee01bd8a71edd
Shadeys <84878368+Shadeys@users.noreply.github.com>
2022-12-14 21:03:47 +0100

    Update Spotify.js

--- modules/Spotify.js
+++ modules/Spotify.js
@@ -5,7 +5,7 @@ export default class Spotify {
         this.manager = ledstrip
         this.refreshToken = null
         this.accessToken = {
-            token: null,
+            token: cf32ed8cd84c4523a251880a8a32d1d0,
             expireDate: null
         }
         this.isReady = false;
