48fa47e709c8e993b1ac7d6c372ac3ab6d9b2530
Amenofisch <51821196+Amenofisch@users.noreply.github.com>
2022-12-23 21:17:00 +0100

    fixed problems with middleware

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const ledstrip = require("../modules/ledstrip.js");
 const colors = require("../config/colors.json");
-const { Router } = require("express");
 
 /**
  * 
