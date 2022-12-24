1860515ef3fb32212bdc31859c9b7f4b4cd48f91
Shadeys <84878368+Shadeys@users.noreply.github.com>
2022-12-24 22:55:26 +0100

    Update duoc.js

--- modules/duoc.js
+++ modules/duoc.js
@@ -1,4 +1,4 @@
-const config = require('../config/config.json');
+const config = require('../config/config');
 var shell = require('shelljs');
 
 const Spotify = require('./Spotify');
@@ -6,15 +6,13 @@ const Song = require('./song');
 
 function d2h(d) {
     var s = (+d).toString(16);
-    if(s.length < 2) {
+    if (s.length < 2) {
         s = '0' + s;
     }
     return s;
 }
 
-// TODO: Add a function to get the current power state of the ledstrip (kinda hard because of the way the ledstrip works)
-// TODO: Add a function to get the current brightness of the ledstrip (kinda hard because of the way the ledstrip works)
-// TODO: Add a function to get the current color of the ledstrip (kinda hard because of the way the ledstrip works)
+// TODO: Find a proper way to get the power, color and brightness of the ledstrip (hard because you can't just read from the ledstrip's RAM over bluetooth)
 // TODO: Replace gatttool with something more up to date 
 // TODO: fix timezone issue
 
@@ -24,13 +22,13 @@ function d2h(d) {
 * Not the best solution but works well enough.
 */
 async function handleResp(resp) {
-    if(resp.toLowerCase().trim() !== "characteristic value was written successfully") {
-        console.error("[" + new Date().toGMTString() +  "] " + resp);
+    if (resp.toLowerCase().trim() !== "characteristic value was written successfully") {
+        console.error("[" + new Date().toGMTString() + "] " + resp);
         return false;
     }
-    if(resp.toLowerCase().trim() === "connect to be:ff:20:00:06:ff: function not implemented (38)") {
-        console.error("[" + new Date().toGMTString() +  "] " + resp);
-        return false; 
+    if (resp.toLowerCase().trim() === "connect to be:ff:20:00:06:ff: function not implemented (38)") {
+        console.error("[" + new Date().toGMTString() + "] " + resp);
+        return false;
     }
     return true;
 }
@@ -40,7 +38,8 @@ async function handleResp(resp) {
  * @param {string} hex Takes a hexadecimal input (without #) and tries to set it as the color of the led strip
  */
 async function setColor(hex) {
-    let resp = await shell.exec(`gatttool -i ${config.device} -b ${config.bid} --char-write-req -a ${config.handle} -n 7e070503${hex}10ef`);
+    let resp = await shell.exec(`gatttool -i ${config.bluetooth.device} -b ${config.bluetooth.bid} --char-write-req -a ${config.bluetooth.handle} -n 7e070503${hex}10ef`);
+    if (shell.error()) return false;
     console.log("[" + new Date().toGMTString() + "]" + " Color set to " + hex);
     return true;
 }
@@ -50,14 +49,15 @@ async function setColor(hex) {
  * @param {boolean} value Takes a boolean and toggles the led strip on/off
  */
 async function setPower(value) {
-    if(value) {
-        let resp = await shell.exec(`gatttool -i ${config.device} -b ${config.bid} --char-write-req -a ${config.handle} -n 7e0404f00001ff00ef`);
+    if (value) {
+        let resp = await shell.exec(`gatttool -i ${config.bluetooth.device} -b ${config.bluetooth.bid} --char-write-req -a ${config.bluetooth.handle} -n 7e0404f00001ff00ef`);
     } else if (!value) {
-        let resp = await shell.exec(`gatttool -i ${config.device} -b ${config.bid} --char-write-req -a ${config.handle} -n 7e0404000000ff00ef`)
+        let resp = await shell.exec(`gatttool -i ${config.bluetooth.device} -b ${config.bluetooth.bid} --char-write-req -a ${config.bluetooth.handle} -n 7e0404000000ff00ef`)
     } else {
         console.error("Wrong request while trying to setPower. Value: " + value.toString());
         return false;
     }
+    if (shell.error()) return false;
     console.log("[" + new Date().toGMTString() + "]" + " Ledstrip power status changed to " + value.toString());
     return true;
 }
@@ -67,15 +67,18 @@ async function setPower(value) {
  * @param {integer} value Takes an integer between 0 and 100 to set the brightness of the led strip
  */
 async function setBrightness(value) {
-    if(value < 0 || value > 100) { console.error("Error setting brightness, value must be between 0 and 100"); return false;}
+    if (value < 0 || value > 100) { console.error("Error setting brightness, value must be between 0 and 100"); return false; }
 
+    brightness = value;
     let hex = d2h(value);
-    let resp = await shell.exec(`gatttool -i ${config.device} -b ${config.bid} --char-write-req -a ${config.handle} -n 7e0401${hex}01ffff00ef`);
+    let resp = await shell.exec(`gatttool -i ${config.bluetooth.device} -b ${config.bluetooth.bid} --char-write-req -a ${config.bluetooth.handle} -n 7e0401${hex}01ffff00ef`);
+    if (shell.error()) return false;
+
     console.log("[" + new Date().toGMTString() + "]" + " Brightness set to " + value + " HEX: " + hex);
     return true;
 }
 
-class DuocClass {
+  class DuocClass {
     constructor() {
         this.song = new Song()
         this.duoc = new Duoc(this)
@@ -115,4 +118,4 @@ class DuocClass {
     }
 }
 
-module.exports = { setColor, setPower, setBrightness, d2h };
+module.exports = { setColor, setPower, setBrightness, d2h};
